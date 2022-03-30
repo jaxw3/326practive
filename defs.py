11ab50c59f23e6dd7d5d6f9338bf00402912a886
llwar <102543156+llwar@users.noreply.github.com>
2022-03-30 18:16:28 -0400

    heh

---
+++ defs.py
@@ -0,0 +1,21 @@
+def hellothere():
+    print("hello")
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+def goodbye():
+    print("goodbye")
+=======
+def hellospanish:
+    print("hola")
+>>>>>>> 8213a5162d0a7c49a5a64fc7f053a86e533d9001
+=======
+def hellospanish():
+    print("hola")
+>>>>>>> 97d0ce6dbcaee26ce2cc1d12db4746becafdd101
+
+def goodbye2():
+    print("goodbye")
+    
+def heh():
+    print("heh")
